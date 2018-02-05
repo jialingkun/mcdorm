84fa4aed90c7a85cc0ba6029835ec0ed3412e8a9
Bent Hartz <Bent Hartz>
2018-02-05 14:38:43 +0700

    external CSS on header

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,6 +1,13 @@
 <?php
 class Pages extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('news_model');
+		$this->load->helper('url_helper');
+	}
+
 	public function view($page = 'home')
 	{
 		if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
--- application/views/Templates/header.php
+++ application/views/Templates/header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>CodeIgniter Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
 </head>
 <body>
 
