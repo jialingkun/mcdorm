d2b7c878e315f90e005697097c8b03169081bf05
Kyoko Takkaba <40120kyoko@gmail.com>
2018-03-08 17:25:40 +0700

    insert and edit kamar

--- application/views/main/manajemen_kos_kamar_edit.php
+++ application/views/main/manajemen_kos_kamar_edit.php
@@ -1,108 +1,201 @@
 
- <!-- Page -->
- <div class="page animsition">
+<!-- Page -->
+<div class="page animsition">
   <div class="page-header">
     <h1 class="page-title">Manajemen Kamar</h1>
   </div>
   <div class="page-content">
-<div class="panel">
- <div class="panel-heading">
-  <h3 class="panel-title"><b>Data Kamar Kos</b> Semangka 5</h3>
-</div>
-<div class="panel-body container-fluid">
-  <div class="row row-lg">
-    <div class="col-md-12">
-      <!-- Example Basic Form -->
-      <div class="example-wrap">
-        <div class="example">
-          <form>
-            <div class="form-group row">
-              <div class="col-sm-6">
-                <label class="control-label"><b>Tipe Kamar</b></label>
-                <input type="text" class="form-control"/>
-              </div>
-              <div class="col-sm-6">
-                <label class="control-label"><b>Harga/Bulan</b></label>
-                <input type="text" class="form-control"/>
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-sm-6">
-                <label class="control-label"><b>Panjang (m<sup>2</sup>)</b></label>
-                <input type="text" class="form-control"/>
-              </div>
-              <div class="col-sm-6">
-               <label class="control-label"><b>Lebar (m<sup>2</sup>)</b></label>
-               <input type="text" class="form-control"/>
+    <div class="panel">
+     <div class="panel-heading">
+      <h3 class="panel-title"><b>Data Kamar Kos</b> Semangka 5</h3>
+    </div>
+    <div class="panel-body container-fluid">
+      <div class="row row-lg">
+        <div class="col-md-12">
+          <!-- Example Basic Form -->
+          <div class="example-wrap">
+            <div class="example">
+              <form id="updateData" onsubmit="insertfunction(event)">
+                <div class="form-group row">
+                  <div class="col-sm-6">
+                    <label class="control-label"><b>Tipe Kamar</b></label>
+                    <input type="text" class="form-control" name="nama" id="nama" />
+                  </div>
+                  <div class="col-sm-6">
+                    <label class="control-label"><b>Harga/Bulan</b></label>
+                    <input type="text" class="form-control" name="harga" id="harga"/>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-6">
+                    <label class="control-label"><b>Panjang (m<sup>2</sup>)</b></label>
+                    <input type="text" class="form-control"  name="panjang" id="panjang"/>
+                  </div>
+                  <div class="col-sm-6">
+                   <label class="control-label"><b>Lebar (m<sup>2</sup>)</b></label>
+                   <input type="text" class="form-control" name="lebar" id="lebar"/>
+                 </div>
+               </div>
+               <div class="form-group row">
+                <div class="col-sm-12">
+                  <label class="control-label"><b>Fasilitas</b></label>
+                  <div class="form-group">
+                    <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" name="fasilitas[]" id="fasilitas1" value="AC" />
+                     <label>AC</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" name="fasilitas[]" id="fasilitas2" value="KM Dalam"/>
+                     <label>KM Dalam</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" name="fasilitas[]" id="fasilitas3" value="Lemari"/>
+                     <label>Lemari</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" name="fasilitas[]" id="fasilitas4" value="Kipas Angin"/>
+                     <label>Kipas Angin</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" d name="fasilitas[]" id="fasilitas5" value="Kunci Duplikat"/>
+                     <label>Kunci Duplikat</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" name="fasilitas[]" id="fasilitas6" value="Listrik Token"/>
+                     <label>Listrik Token</label>
+                   </div>
+                 </div>
+               </div>
              </div>
-           </div>
-           <div class="form-group row">
-            <div class="col-sm-12">
-              <label class="control-label"><b>Fasilitas</b></label>
-              <div class="form-group">
-                <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue"/>
-                 <label>AC</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue"/>
-                 <label>KM Dalam</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue"/>
-                 <label>Lemari</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue"/>
-                 <label>Kipas Angin</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue"/>
-                 <label>Kunci Duplikat</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue"/>
-                 <label>Listrik Token</label>
-               </div>
-             </div>
-           </div>
-         </div>
-          <div class="form-group row">
+             <div class="form-group row">
               <div class="col-sm-6">
                 <label class="control-label"><b>Jumlah Kamar</b></label>
-                <input type="number" class="form-control"/>
+                <input type="number" class="form-control"  name="kuota" id="kuota"/>
               </div>
-           </div>
-      </form>
+            </div>
+            <div class="form-group pull-right" style="margin-top: 25px;">
+              <button type="submit" id="submitButton" class="btn btn-animate btn-animate-side btn-info btn-md" >
+                <span><i class="icon fa-plus"></i> &nbsp<b id="submit">Tambahkan Data</b></span>
+              </button>
+              <button type="reset" class="btn btn-animate btn-animate-side btn-warning btn-md">
+                <span><i class="icon fa-refresh"></i> &nbsp<b>Refresh</b></span>
+              </button>
+              <a href="manajemen_kos_data.php">
+                <button type="button" class="btn btn-animate btn-animate-side btn-primary btn-md">
+                  <span><i class="icon fa-mail-reply"></i> &nbsp<b>Kembali</b></span>
+                </button>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- End Example Basic Form -->
     </div>
   </div>
-  <!-- End Example Basic Form -->
-</div>
-</div>
-<form class="upload-form" id="exampleUploadForm" method="POST" style="margin-top: -80px;">
-  <input type="file" id="inputUpload" name="files[]" multiple="" />
-  <div class="uploader-inline">
-    <h1 class="upload-instructions">Upload foto / gambar</h1>
-  </div>
-  <div class="file-wrap container-fluid">
-    <div class="file-list row"></div>
-  </div>
-</form>
-<div class="form-group pull-right" style="margin-top: 25px;">
-  <button type="submit" class="btn btn-animate btn-animate-side btn-info btn-md">
-    <span><i class="icon fa-plus"></i> &nbsp<b>Tambahkan Data</b></span>
-  </button>
-  <button type="reset" class="btn btn-animate btn-animate-side btn-warning btn-md">
-    <span><i class="icon fa-refresh"></i> &nbsp<b>Refresh</b></span>
-  </button>
-  <a href="manajemen_kos_data.php">
-    <button type="button" class="btn btn-animate btn-animate-side btn-primary btn-md">
-      <span><i class="icon fa-mail-reply"></i> &nbsp<b>Kembali</b></span>
-    </button>
-  </a>
+  <form class="upload-form" id="exampleUploadForm" method="POST" style="margin-top: -80px;">
+    <input type="file" id="inputUpload" name="files[]" multiple="" />
+    <div class="uploader-inline">
+      <h1 class="upload-instructions">Upload foto / gambar</h1>
+    </div>
+    <div class="file-wrap container-fluid">
+      <div class="file-list row"></div>
+    </div>
+  </form>
+  
 </div>
 </div>
 </div>
 </div>
-</div>
+
+
+<script>
+  window.onload = function() {
+
+    var urls='main/getkamar/'+getCookie("editDataKos")+'/'+getCookie("editDataKamar")+'';
+    
+    $.ajax({
+      url:"<?php echo base_url() ?>index.php/"+urls,
+      type: 'get',
+      dataType: "json",
+      success: function (response) {
+        $('#nama').val(response.nama_kamar);
+        $('#harga').val(response.harga);
+        $('#panjang').val(response.panjang);
+        $('#lebar').val(response.lebar);
+        $('#kuota').val(response.kuota);
+
+          // $('#fasilitas').html(response.fasilitas_kos);
+          var fas = response.fasilitas_kamar;
+          var res = fas.split(",");
+
+          for (var i = 0; i < res.length; i++) {
+            // alert( res[i].replace(/\s/g, ''));
+            
+            if ($("#fasilitas1").val()==res[i]) {
+              $("#fasilitas1").attr('checked', true);
+            }
+            else if ($("#fasilitas2").val()==res[i]) {
+              $("#fasilitas2").attr('checked', true);
+            }
+            else if ($("#fasilitas3").val()==res[i]) {
+              $("#fasilitas3").attr('checked', true);
+            }
+            else if ($("#fasilitas4").val()==res[i]) {
+              $("#fasilitas4").attr('checked', true);
+            }
+            else if ($("#fasilitas5").val()==res[i]) {
+              $("#fasilitas5").attr('checked', true);
+            }
+            else if ($("#fasilitas6").val()==res[i]) {
+              $("#fasilitas6").attr('checked', true);
+            }
+          }
+          
+        }
+      });
+  }
+
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+
+  function insertfunction(e) {
+    var urls='main/updatekamar/'+getCookie("editDataKos")+'/'+getCookie("editDataKamar")+'';
+  e.preventDefault();// will stop the form submission
+  var buttonname = $("#submit").val();
+  $("#submit").html("Tunggu...");
+  $("#submitButton").prop("disabled",true);
+  $.ajax({
+    url:"<?php echo base_url() ?>index.php/"+urls,
+    type: 'POST',
+    data: $("#updateData").serialize(),
+    success: function(response){
+      if (response == 1) {
+        window.location.href = 'manajemen_kos_edit';
+        $("#submit").html(buttonname);
+      }else{
+        // $("#submit").val(buttonname);
+        alert(response);
+        $("#submit").html(buttonname);
+      }
+    }
+  });   
+}
+
+</script>
 <!-- End Page -->
--- application/views/main/manajemen_kos_kamar_insert.php
+++ application/views/main/manajemen_kos_kamar_insert.php
@@ -1,106 +1,107 @@
 
- <!-- Page -->
- <div class="page animsition">
+<!-- Page -->
+<div class="page animsition">
   <div class="page-header">
     <h1 class="page-title">Manajemen Kamar</h1>
   </div>
   <div class="page-content">
-<div class="panel">
- <div class="panel-heading">
-  <h3 class="panel-title"><b>Data Kamar Kos</b> Semangka 5</h3>
-</div>
-<div class="panel-body container-fluid">
-  <div class="row row-lg">
-    <div class="col-md-12">
-      <!-- Example Basic Form -->
-      <div class="example-wrap">
-        <div class="example">
-          <form id="insertData">
-            <div class="form-group row">
-              <div class="col-sm-6">
-                <label class="control-label"><b>Tipe Kamar</b></label>
-                <input type="text" class="form-control" name="nama" />
-              </div>
-              <div class="col-sm-6">
-                <label class="control-label"><b>Harga/Bulan</b></label>
-                <input type="text" class="form-control" name="harga" />
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-sm-6">
-                <label class="control-label"><b>Panjang (m<sup>2</sup>)</b></label>
-                <input type="text" class="form-control" name="panjang" />
-              </div>
-              <div class="col-sm-6">
-               <label class="control-label"><b>Lebar (m<sup>2</sup>)</b></label>
-               <input type="text" class="form-control" name="lebar" />
+    <div class="panel">
+     <div class="panel-heading">
+      <h3 class="panel-title"><b>Data Kamar Kos</b> Semangka 5</h3>
+    </div>
+    <div class="panel-body container-fluid">
+      <div class="row row-lg">
+        <div class="col-md-12">
+          <!-- Example Basic Form -->
+          <div class="example-wrap">
+            <div class="example">
+              <form id="insertData">
+                <div class="form-group row">
+                  <div class="col-sm-6">
+                    <label class="control-label"><b>Tipe Kamar</b></label>
+                    <input type="text" class="form-control" name="nama" />
+                  </div>
+                  <div class="col-sm-6">
+                    <label class="control-label"><b>Harga/Bulan</b></label>
+                    <input type="text" class="form-control" name="harga" />
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-6">
+                    <label class="control-label"><b>Panjang (m<sup>2</sup>)</b></label>
+                    <input type="text" class="form-control" name="panjang" />
+                  </div>
+                  <div class="col-sm-6">
+                   <label class="control-label"><b>Lebar (m<sup>2</sup>)</b></label>
+                   <input type="text" class="form-control" name="lebar" />
+                 </div>
+               </div>
+               <div class="form-group row">
+                <div class="col-sm-12">
+                  <label class="control-label"><b>Fasilitas</b></label>
+                  <div class="form-group">
+                    <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" value="AC" />
+                     <label>AC</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" value="KM Dalam" />
+                     <label>KM Dalam</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" value="Lemari" />
+                     <label>Lemari</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" value="Kipas Angin" />
+                     <label>Kipas Angin</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" value="Kunci Duplikat" />
+                     <label>Kunci Duplikat</label>
+                   </div>
+                   <div class="col-sm-2">
+                     <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" value="Listrik Token" />
+                     <label>Listrik Token</label>
+                   </div>
+                 </div>
+               </div>
              </div>
-           </div>
-           <div class="form-group row">
-            <div class="col-sm-12">
-              <label class="control-label"><b>Fasilitas</b></label>
-              <div class="form-group">
-                <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]" />
-                 <label>AC</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]"/>
-                 <label>KM Dalam</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]"/>
-                 <label>Lemari</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]"/>
-                 <label>Kipas Angin</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]"/>
-                 <label>Kunci Duplikat</label>
-               </div>
-               <div class="col-sm-2">
-                 <input type="checkbox" class="icheckbox-primary" data-plugin="iCheck" data-checkbox-class="icheckbox_flat-blue" name="fasilitas[]"/>
-                 <label>Listrik Token</label>
-               </div>
-             </div>
-           </div>
-         </div>
-          <div class="form-group row">
+             <div class="form-group row">
               <div class="col-sm-6">
                 <label class="control-label"><b>Jumlah Kamar</b></label>
                 <input type="number" class="form-control" name="kuota" />
               </div>
-           </div>
-      </form>
+            </div>
+            <div class="form-group pull-right" style="margin-top: 25px;">
+              <button type="submit" id="submitButton" class="btn btn-animate btn-animate-side btn-info btn-md" onclick="insertDataKamar()">
+                <span><i class="icon fa-plus"></i> &nbsp<b id="submit">Tambahkan Data</b></span>
+              </button>
+              <button type="reset" class="btn btn-animate btn-animate-side btn-warning btn-md">
+                <span><i class="icon fa-refresh"></i> &nbsp<b>Refresh</b></span>
+              </button>
+              <a href="manajemen_kos_data.php">
+                <button type="button" class="btn btn-animate btn-animate-side btn-primary btn-md">
+                  <span><i class="icon fa-mail-reply"></i> &nbsp<b>Kembali</b></span>
+                </button>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- End Example Basic Form -->
     </div>
   </div>
-  <!-- End Example Basic Form -->
-</div>
-</div>
-<form class="upload-form" id="exampleUploadForm" method="POST" style="margin-top: -80px;">
-  <input type="file" id="inputUpload" name="files[]" multiple="" />
-  <div class="uploader-inline">
-    <h1 class="upload-instructions">Upload foto / gambar</h1>
-  </div>
-  <div class="file-wrap container-fluid">
-    <div class="file-list row"></div>
-  </div>
-</form>
-<div class="form-group pull-right" style="margin-top: 25px;">
-  <button type="submit" id="submitButton" class="btn btn-animate btn-animate-side btn-info btn-md">
-    <span><i class="icon fa-plus"></i> &nbsp<b id="submit">Tambahkan Data</b></span>
-  </button>
-  <button type="reset" class="btn btn-animate btn-animate-side btn-warning btn-md">
-    <span><i class="icon fa-refresh"></i> &nbsp<b>Refresh</b></span>
-  </button>
-  <a href="manajemen_kos_data.php">
-    <button type="button" class="btn btn-animate btn-animate-side btn-primary btn-md">
-      <span><i class="icon fa-mail-reply"></i> &nbsp<b>Kembali</b></span>
-    </button>
-  </a>
-</div>
+  <form class="upload-form" id="exampleUploadForm" method="POST" style="margin-top: -80px;">
+    <input type="file" id="inputUpload" name="files[]" multiple="" />
+    <div class="uploader-inline">
+      <h1 class="upload-instructions">Upload foto / gambar</h1>
+    </div>
+    <div class="file-wrap container-fluid">
+      <div class="file-list row"></div>
+    </div>
+  </form>
+
 </div>
 </div>
 </div>
@@ -112,7 +113,7 @@
 
   function insertDataKamar() {
     var urls='main/insertKamar/'+getCookie("editDataKos")+"";
-
+    alert(urls);
     var dataString = $("#insertData").serialize();
     var buttonname = $("#submit").val();
     $("#submit").html("Tunggu...");
@@ -137,18 +138,18 @@
   }
 
   function getCookie(cname) {
-      var name = cname + "=";
-      var ca = document.cookie.split(';');
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
 
-      for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-        }
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
       }
-      return "";
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
+    return "";
+  }
 </script>
